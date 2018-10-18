2aa5d4760c8a90f3bb0806dc68f19648cb6d65ab
Allen Zhou <jz1749@nyu.edu>
2018-10-18 14:35:24 +0800

    Clean code in dgl capsule layers

--- dgl_capsule_batch.py
+++ dgl_capsule_batch.py
@@ -11,34 +11,6 @@ from utils import writer, step
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-class DGLFeature():
-    """
-    To wrap different shape of representation tensor into the same shape
-    """
-
-    def __init__(self, tensor, pad_to):
-        # self.tensor = tensor
-        self.node_num = tensor.size(0)
-        self.flat_tensor = tensor.contiguous().view(self.node_num, -1)
-        self.node_feature_dim = self.flat_tensor.size(1)
-        self.flat_pad_tensor = F.pad(self.flat_tensor, (0, pad_to - self.flat_tensor.size(1)))
-        self.shape = tensor.shape
-
-    @property
-    def tensor(self):
-        """
-        :return: Tensor with original shape
-        """
-        return self.flat_tensor.index_select(1, torch.arange(0, self.node_feature_dim).to(device)).view(self.shape)
-
-    @property
-    def padded_tensor(self):
-        """
-        :return: Flatted and padded Tensor
-        """
-        return self.flat_pad_tensor
-
-
 class DGLBatchCapsuleLayer(CapsuleLayer):
     def __init__(self, in_unit, in_channel, num_unit, unit_size, use_routing,
                  num_routing, cuda_enabled):
@@ -47,69 +19,55 @@ class DGLBatchCapsuleLayer(CapsuleLayer):
         self.unit_size = unit_size
         self.weight = nn.Parameter(torch.randn(in_channel, num_unit, unit_size, in_unit))
 
+        ### Construct Grapu
         self.g = dgl.DGLGraph()
 
-        self.g.add_nodes_from([i for i in range(self.in_channel)])
-        self.g.add_nodes_from([i + self.in_channel for i in range(self.num_unit)])
-        for i in range(self.in_channel):
-            for j in range(self.num_unit):
-                index_j = j + self.in_channel
-                self.g.add_edge(i, index_j)
+        self.g.add_nodes(self.in_channel + self.num_unit)
+        self.in_channel_nodes = list(range(self.in_channel))
+        self.capsule_nodes = list(range(self.in_channel, self.in_channel + self.num_unit))
+        u, v = [], []
+        for i in self.in_channel_nodes:
+            for j in self.capsule_nodes:
+                u.append(i)
+                v.append(j)
+
+        self.g.add_edges(u, v)
 
     def routing(self, x):
 
         self.batch_size = x.size(0)
 
-        self.edge_features = torch.zeros(self.in_channel, self.num_unit).to('cuda')
-
-        x_ = x.transpose(0, 2)
-        x_ = DGLFeature(x_, self.batch_size * self.unit_size)
-
         x = x.transpose(1, 2)
         x = torch.stack([x] * self.num_unit, dim=2).unsqueeze(4)
-        W = torch.cat([self.weight.unsqueeze(0)] * self.batch_size, dim=0)
+        W = self.weight.expand(self.batch_size, *self.weight.shape)
+
+        # [1152, 10, 128, 16]
         u_hat = torch.matmul(W, x).permute(1, 2, 0, 3, 4).squeeze().contiguous()
 
-        self.node_feature = DGLFeature(torch.zeros(self.num_unit, self.batch_size, self.unit_size).to('cuda'),
-                                       self.batch_size * self.unit_size)
-        nf = torch.cat([x_.padded_tensor, self.node_feature.padded_tensor], dim=0)
+        self.node_feature = torch.zeros(self.in_channel + self.num_unit, self.batch_size, self.unit_size).to(device)
+
+        self.edge_features = torch.zeros(self.in_channel, self.num_unit).to('cuda')
 
         self.g.set_e_repr({'b_ij': self.edge_features.view(-1)})
-        self.g.set_n_repr({'h': nf})
+        self.g.set_n_repr({'h': self.node_feature})
         self.g.set_e_repr({'u_hat': u_hat.view(-1, self.batch_size, self.unit_size)})
 
         for i in range(self.num_routing):
             self.i = i
-            self.g.update_all(self.capsule_msg, self.capsule_reduce, self.update_node, batchable=True)
-            # self.g.update_all(self.capsule_msg, self.v2_reduce, self.v2_update, batchable=True)
-            self.g.update_edge(dgl.base.ALL, dgl.base.ALL, self.update_edge, batchable=True)
+            self.g.update_all(self.capsule_msg, self.v2_reduce, self.v2_update)
+            self.g.update_edge(edge_func=self.update_edge)
 
-        self.node_feature = self.g.get_n_repr()['h'] \
+        capsule_node_feature = self.g.get_n_repr()['h'] \
             .index_select(0, torch.arange(self.in_channel, self.in_channel + self.num_unit).to(device)) \
             .view(self.num_unit, self.batch_size, self.unit_size)
-        return self.node_feature.transpose(0, 1).unsqueeze(1).unsqueeze(4).squeeze(1)
-
-    def update_node(self, x):
-        return {'h': DGLFeature(x['h'], self.batch_size * self.unit_size).padded_tensor}
+        return capsule_node_feature.transpose(0, 1).unsqueeze(1).unsqueeze(4).squeeze(1)
 
     def update_edge(self, u, v, edge):
-        return {
-            'b_ij': edge['b_ij'] + (v['h'].view(-1, self.batch_size, self.unit_size) * edge['u_hat']).mean(dim=1).sum(
-                dim=1)
-        }
+        return {'b_ij': edge['b_ij'] + (v['h'] * edge['u_hat']).mean(dim=1).sum(dim=1)}
 
     @staticmethod
     def capsule_msg(src, edge):
         return {'b_ij': edge['b_ij'], 'h': src['h'], 'u_hat': edge['u_hat']}
-
-    def capsule_reduce(self, node, msg):
-        b_ij_c, h_c, u_hat_c = msg['b_ij'], msg['h'], msg['u_hat']
-        u_hat = u_hat_c
-        c_i = F.softmax(b_ij_c, dim=0)
-        writer.add_histogram(f"c_i{self.i}", c_i, step['step'])
-        s_j = (c_i.unsqueeze(2).unsqueeze(3) * u_hat).sum(dim=1)
-        v_j = self.squash(s_j)
-        return {'h': v_j.view(-1, self.batch_size * self.unit_size)}
 
     @staticmethod
     def squash(s):
@@ -120,17 +78,12 @@ class DGLBatchCapsuleLayer(CapsuleLayer):
         return s
 
     def v2_reduce(self, node, msg):
-        b_ij_c, h_c, u_hat_c = msg['b_ij'], msg['h'], msg['u_hat']
-        u_hat = u_hat_c
+        b_ij_c, u_hat = msg['b_ij'], msg['u_hat']
         c_i = F.softmax(b_ij_c, dim=0)
         writer.add_histogram(f"c_i{self.i}", c_i, step['step'])
         s_j = (c_i.unsqueeze(2).unsqueeze(3) * u_hat).sum(dim=1)
-        return {'h': s_j.view(-1, self.batch_size * self.unit_size)}
+        return {'h': s_j}
 
     def v2_update(self, msg):
-        pre_layer_msgs = msg['h'][:1152]
-        msg = msg['h'][1152:].view(self.num_unit, self.batch_size, self.unit_size)
-        v_j = self.squash(msg)
-        return {
-            'h': DGLFeature(torch.cat([pre_layer_msgs, v_j.view(self.num_unit, -1)], dim=0),
-                            self.batch_size * self.unit_size).padded_tensor}
+        v_j = self.squash(msg['h'])
+        return {'h': v_j}
