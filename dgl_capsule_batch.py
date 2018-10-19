ff929327087726f67167e36996047a5ea758a9a2
Allen Zhou <jz1749@nyu.edu>
2018-10-19 15:57:35 +0800

    clean code

--- dgl_capsule_batch.py
+++ dgl_capsule_batch.py
@@ -4,10 +4,7 @@ import torch.nn.functional as F
 from torch import nn
 
 from capsule_layer import CapsuleLayer
-# import main
-from utils import writer, step
 
-# global_step = main.global_step
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
@@ -19,7 +16,7 @@ class DGLBatchCapsuleLayer(CapsuleLayer):
         self.unit_size = unit_size
         self.weight = nn.Parameter(torch.randn(in_channel, num_unit, unit_size, in_unit))
 
-        ### Construct Grapu
+        ### Construct Graph
         self.g = dgl.DGLGraph()
 
         self.g.add_nodes(self.in_channel + self.num_unit)
@@ -44,22 +41,19 @@ class DGLBatchCapsuleLayer(CapsuleLayer):
         # [1152, 10, 128, 16]
         u_hat = torch.matmul(W, x).permute(1, 2, 0, 3, 4).squeeze().contiguous()
 
-        self.node_feature = torch.zeros(self.in_channel + self.num_unit, self.batch_size, self.unit_size).to(device)
+        node_features = torch.zeros(self.in_channel + self.num_unit, self.batch_size, self.unit_size).to(device)
 
-        self.edge_features = torch.zeros(self.in_channel, self.num_unit).to('cuda')
+        edge_features = torch.zeros(self.in_channel, self.num_unit).to(device)
 
-        self.g.set_e_repr({'b_ij': self.edge_features.view(-1)})
-        self.g.set_n_repr({'h': self.node_feature})
+        self.g.set_e_repr({'b_ij': edge_features.view(-1)})
+        self.g.set_n_repr({'h': node_features})
         self.g.set_e_repr({'u_hat': u_hat.view(-1, self.batch_size, self.unit_size)})
 
         for i in range(self.num_routing):
-            self.i = i
             self.g.update_all(self.capsule_msg, self.v2_reduce, self.v2_update)
             self.g.update_edge(edge_func=self.update_edge)
 
-        capsule_node_feature = self.g.get_n_repr()['h'] \
-            .index_select(0, torch.arange(self.in_channel, self.in_channel + self.num_unit).to(device)) \
-            .view(self.num_unit, self.batch_size, self.unit_size)
+        capsule_node_feature = self.g.get_n_repr()['h'][self.in_channel:self.in_channel + self.num_unit]
         return capsule_node_feature.transpose(0, 1).unsqueeze(1).unsqueeze(4).squeeze(1)
 
     def update_edge(self, u, v, edge):
@@ -71,16 +65,15 @@ class DGLBatchCapsuleLayer(CapsuleLayer):
 
     @staticmethod
     def squash(s):
-        # This is equation 1 from the paper.
         mag_sq = torch.sum(s ** 2, dim=2, keepdim=True)
         mag = torch.sqrt(mag_sq)
         s = (mag_sq / (1.0 + mag_sq)) * (s / mag)
         return s
 
-    def v2_reduce(self, node, msg):
+    @staticmethod
+    def v2_reduce(node, msg):
         b_ij_c, u_hat = msg['b_ij'], msg['u_hat']
         c_i = F.softmax(b_ij_c, dim=0)
-        writer.add_histogram(f"c_i{self.i}", c_i, step['step'])
         s_j = (c_i.unsqueeze(2).unsqueeze(3) * u_hat).sum(dim=1)
         return {'h': s_j}
 
